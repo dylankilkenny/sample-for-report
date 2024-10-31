4c376990b6e300ea714a6bd4133ee05e6f4e4b94
dylankilkenny <dylankilkenny95@gmail.com>
2024-10-31 00:28:54 +0000

    Update MyToken.sol

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.9;
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Supply.sol";
 
-//test12345
+//test123457
 contract MyToken is ERC1155, ERC1155Supply {
     constructor() ERC1155("") {}
 
